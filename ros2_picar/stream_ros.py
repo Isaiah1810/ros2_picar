9ddcfed576e70154dd9b5ac657b0a747a44355e9
Isaiah1810 <isaiah1810@gmail.com>
2024-04-03 05:01:36 +0800

    added second feed

--- ros2_picar/stream_ros.py
+++ ros2_picar/stream_ros.py
@@ -13,6 +13,7 @@ import numpy as np
 
 # RTSP server configuration
 RTSP_URL = "rtsp://localhost:8554/live"
+RTSP_URL2 = "rtsp://localhost:8554/live_yolo"
 WIDTH = 640
 HEIGHT = 480
 
@@ -27,14 +28,15 @@ class ImageSubscriber(Node):
 
         # Start the ffmpeg process to create the RTSP server
         ffmpeg_command = ['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'bgr24', '-s', '{}x{}'.format(WIDTH, HEIGHT), '-i', '-', '-rtsp_flags', 'listen', '-f', 'rtsp', RTSP_URL]
+        ffmpeg_command2 = ['ffmpeg', '-f', 'rawvideo', '-pix_fmt', 'bgr24', '-s', '{}x{}'.format(WIDTH, HEIGHT), '-i', '-', '-rtsp_flags', 'listen', '-f', 'rtsp', RTSP_URL2]
         self.ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
-
+        self.ffmpeg_process2 = subprocess.Popen(ffmpeg_command2, stdin=subprocess.PIPE)
     def image_callback(self, msg):
-        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         self.publish_to_stream()
 
     def yolo_callback(self, msg):
-        self.yolo = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.yolo = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         self.publish_to_stream()
 
     def publish_to_stream(self):
@@ -44,7 +46,8 @@ class ImageSubscriber(Node):
         #concatenated_image = np.concatenate((self.yolo, self.image), axis=1)
 
         # Write concatenated image to ffmpeg process stdin
-        self.ffmpeg_process.stdin.write(self.yolo.tobytes())
+        self.ffmpeg_process.stdin.write(self.image.tobytes())
+        self.ffmpeg_process2.stdin.write(self.yolo.tobytes())
 
 def main():
     try:
